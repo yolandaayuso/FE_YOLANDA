5a3b84c5be8cd0a5b61e515c424b749089b14318
yolandaayuso <114437997+yolandaayuso@users.noreply.github.com>
2023-04-24 12:20:36 +0200

    Update payment.service.ts

--- number-match/src/app/payment.service.ts
+++ number-match/src/app/payment.service.ts
@@ -1,112 +1,26 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-declare let Stripe: any;
+import { Observable } from 'rxjs';
+/*import { WebSocketSubject } from 'rxjs/webSocket';*/
+
 @Injectable({
   providedIn: 'root'
 })
-export class PaymentComponent {
+export class PaymentService {
 
-  stripe = Stripe("pk_test_51MqBSKH7OM2285cjwfHb0Vw5mrIDQDS9F0SjZaPdp0DEUznmHxJtL17uT9bfjW75rSHu2bK9kFip2JpFANk7p4UQ00OrH3vfiH")
-  token?: string
+  private paymentUrl = 'http://localhost:8081/payments/paymentOk';
+
+  constructor(private httpClient : HttpClient) { }
 
 
-constructor() { }
-
-  ngOnInit(): void {
+  pay(amount: number): Observable<string> {
+    return this.httpClient.get(`http://localhost:8081/payments/prepay?amount=${amount}`, {responseType: 'text'});
   }
 
-
-
-
-
-pay(){
-let self = this
-let req = new XMLHttpRequest()
-req.open("GET", "http://localhost/payments/prepay?amount=100", true)
-req.onreadystatechange = function (response) {
-  if (req.readyState == 4) {
-    if (req.status == 200) {
-      self.token = req.responseText
-      self.showForm()
-    }
-    else {
-      alert(req.statusText)
-    }
+  paymentOk(token: string): Observable<any> {
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
+    const payload = { token };
+    return this.httpClient.post(this.paymentUrl, payload, { headers });
   }
 }
-req.send()
-}
 
-showForm(){
-  let elements = this.stripe.elements() //le dira a stripe que hay un objeto en ese elemento que se ha bajado
-  let style = {
-  base: {
-  color: "#32325d", fontFamily: 'Arial, sans-serif',
-  fontSmoothing: "antialiased", fontSize: "16px",
-    "::placeholder": {
-color: "#32325d"
-}
-    },invalid: {
-  fontFamily: 'Arial, sans-serif', color: "#fa755a",
-  iconColor: "#fa755a"
-  }
-  }
-  let card = elements.create("card", { style : style }) //CREARA UN ELEMTNO LLAMADO CAR ELEMTN OTRO PAYment form
-  card.mount("#card-element")
-  card.on("change", function(event : any) {
-  document.querySelector("button")!.disabled = event.empty;
-  document.querySelector("#card-error")!.textContent =
-  event.error ? event.error.message : "";
-  });
-  let self = this
-  let form = document.getElementById("payment-form"); form!.addEventListener("submit", function(event) {
-  event.preventDefault();
-  self.payWithCard(card);
-  });
-  form!.style.display = "block"
-}
-
-
-  payWithCard(card: any) {
-    let self = this
-    this.stripe.confirmCardPayment(this.token, {
-      payment_method: {
-        card: card
-      }
-    }).then(function (response: any) {
-      if (response.error) {
-        alert(response.error.message);
-      } else {
-        if (response.paymentIntent.status === 'succeeded') {
-          alert("Pago exitoso");
-          self.paymentOK();
-
-
-        }
-      }
-    });
-  }
-  paymentOK() {
-    let self = this
-    let payload={
-      token: this.token
-    }
-    let req = new XMLHttpRequest()
-    req.open("POST", "http://localhost:8081/payments/paymentOK")//SI FUERA UN GET LLEVARIA EL TOKEN AQUI, COMO ES UN POST, LO LLLEVA EN EL PAYLOAD
-    req.setRequestHeader("Content-Type", "application/json")
-    req.onreadystatechange=function(response){//cuando cambies el state, ejecuta esta funcion, con el response que te devuelve el servidor, el token
-      if(req.readyState==4  ){ //si el state es 4 , es que la peticion ha ido bien
-        if(req.status==200){ //si el status es 200, es que la peticion ha ido bien
-          alert("tu pago ha sido completado") // en el paymentController tendremos un metodo en el que el recurso sea paymentOK, y que devuelva un OK
-      }else{
-        alert(req.statusText)
-      }
-      }
-    }
-    req.send(JSON.stringify(payload))
-
-  }
-
-  pagar(){
-
-  }
-}
