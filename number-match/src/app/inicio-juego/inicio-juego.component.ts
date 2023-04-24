8af2240488199ef4ec3b52591cc6d87c86ff0650
yolandaayuso <114437997+yolandaayuso@users.noreply.github.com>
2023-04-24 12:19:03 +0200

    Update inicio-juego.component.ts

--- number-match/src/app/inicio-juego/inicio-juego.component.ts
+++ number-match/src/app/inicio-juego/inicio-juego.component.ts
@@ -33,14 +33,14 @@ export class InicioJuegoComponent implements OnInit{
   mostrarMenu: boolean = true
   iniciarPago: boolean = false
   iniciarJuegoGratis: boolean = false
-
+  mostrarFormulario: boolean = true
   tableroPadre : any
   id? : string
   contador : number = 0;
   x1 : number = 0
   y1 : number = 0
+  token? : string
 
-  token?: string
 
 
   stripe = Stripe("pk_test_51MqBSKH7OM2285cjwfHb0Vw5mrIDQDS9F0SjZaPdp0DEUznmHxJtL17uT9bfjW75rSHu2bK9kFip2JpFANk7p4UQ00OrH3vfiH")
@@ -54,7 +54,7 @@ export class InicioJuegoComponent implements OnInit{
 
   }
 
-  constructor(private GameService : GameService,private accountService: AccountService) { }
+  constructor(private GameService : GameService,private accountService: AccountService, private PaymentService: PaymentService) { }
 
   instruccionesVisible = false;
 
@@ -68,6 +68,15 @@ export class InicioJuegoComponent implements OnInit{
 
   toggleInstrucciones() {
     this.instruccionesVisible = !this.instruccionesVisible;
+  }
+
+
+  toPrincipal(){
+    this.mostrarElemento = true;
+
+    // Reseteo de los valores de las variables
+    this.iniciarJuegoGratis = false;
+    this.iniciarPago = false;
   }
 
 
@@ -172,32 +181,23 @@ export class InicioJuegoComponent implements OnInit{
     })
   }
 
+pay(){
+this.PaymentService.pay(2).subscribe((token) => {
+  this.token = token;
+  this.showForm();
+}, (error) => {
+    Swal.fire({
+      title: 'Error',
+      text: error.error.message,
+      icon: 'error',
+      confirmButtonText: 'Aceptar'
+    });
+});
+}
 
 
+showForm(){
 
-
-
-
-
-  pay(){
-    let self = this
-    let req = new XMLHttpRequest()
-    req.open("GET", "http://localhost:8081/payments/prepay?amount=100", true)
-    req.onreadystatechange = function (response) {
-      if (req.readyState == 4) {
-        if (req.status == 200) {
-          self.token = req.responseText
-          self.showForm()
-        }
-        else {
-          alert(req.statusText)
-        }
-      }
-    }
-    req.send()
-    }
-
-    showForm(){
       let elements = this.stripe.elements() //le dira a stripe que hay un objeto en ese elemento que se ha bajado
       let style = {
       base: {
@@ -222,12 +222,13 @@ export class InicioJuegoComponent implements OnInit{
       let form = document.getElementById("payment-form"); form!.addEventListener("submit", function(event) {
       event.preventDefault();
       self.payWithCard(card);
+
       });
       form!.style.display = "block"
-    }
+}
 
 
-      payWithCard(card: any) {
+payWithCard(card: any) {
         let self = this
         this.stripe.confirmCardPayment(this.token, {
           payment_method: {
@@ -235,56 +236,48 @@ export class InicioJuegoComponent implements OnInit{
           }
         }).then(function (response: any) {
           if (response.error) {
-            alert(response.error.message);
+            Swal.fire({
+              title: 'Error',
+              text: response.error.message,
+              icon: 'error',
+              confirmButtonText: 'Aceptar'
+            });
+
           } else {
             if (response.paymentIntent.status === 'succeeded') {
-              alert("Pago exitoso");
               self.paymentOk();
 
 
             }
           }
         });
-      }
-      paymentOk() {
+}
+paymentOk() {
         let self = this
+
         let payload={
-          token: this.token
-        }
-        let req = new XMLHttpRequest()
-        req.open("POST", "http://localhost:8081/payments/paymentOK")//SI FUERA UN GET LLEVARIA EL TOKEN AQUI, COMO ES UN POST, LO LLLEVA EN EL PAYLOAD
-        req.setRequestHeader("Content-Type", "application/json")
-        req.onreadystatechange=function(response){//cuando cambies el state, ejecuta esta funcion, con el response que te devuelve el servidor, el token
-          if(req.readyState==4  ){ //si el state es 4 , es que la peticion ha ido bien
-            if(req.status==200){ //si el status es 200, es que la peticion ha ido bien
-              alert("tu pago ha sido completado") // en el paymentController tendremos un metodo en el que el recurso sea paymentOK, y que devuelva un OK
-          }else{
-            alert(req.statusText)
-          }
-          }
-        }
-        req.send(JSON.stringify(payload))
+          token: this.token ?? '' // si no existe token, que sea un string vacio
 
-      }
+        }
 
+        this.PaymentService.paymentOk(payload.token).subscribe((data : any) => {
+          Swal.fire({
+            title: 'Tu pago ha sido completado',
+            text: '¡Ya puedes retarte en la versión premium!',
+            icon: 'success',
+            confirmButtonText: 'Aceptar'
+          });
+        },
+        error => {
+          console.log(error)
+        });
 }
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 
 
